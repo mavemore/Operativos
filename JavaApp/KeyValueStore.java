cf9150ba9adf4d1a50f218b5c8dee1c99bd894c2
mavemore <veronica940413@hotmail.com>
2016-07-07 01:54:15 -0500

    Puerto pasado por argumento
    servidor

--- JavaApp/KeyValueStore.java
+++ JavaApp/KeyValueStore.java
@@ -1,17 +1,28 @@
 import java.io.*;
+import static java.lang.Integer.parseInt;
 import java.net.*;
 
 public class KeyValueStore {
-    static final int PORT = 6789;
+    static int port;
 
     public static void main(String args[]) {
         ServerSocket serverSocket = null;
         Socket socket = null;
-
+        try{
+            String puerto = args[0];
+            if(!puerto.matches("[0-9]+") && puerto.length() < 5){
+                System.out.println("ERROR: argumento no valido.");
+                return;
+            }
+        }catch(Exception e){
+            System.out.println("ERROR: argumento no valido.");
+            return;
+        }
         try {
-            serverSocket = new ServerSocket(PORT);
-        } catch (IOException e) {
+            serverSocket = new ServerSocket(parseInt(args[0]));
+        } catch (Exception e) {
             System.out.println("ERROR: Problemas al abrir el socket.");
+            return;
 
         }
         while (true) {
@@ -19,6 +30,7 @@ public class KeyValueStore {
                 socket = serverSocket.accept();
             } catch (IOException e) {
                 System.out.println("ERROR: No pudo establecer la conexion con el cliente.");
+                return;
             }
             // new threa for a client
             new TCPServer(socket).start();
